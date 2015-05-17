37c67208455ff320fe5431df1bbed56e8a172975
sunthia <sathi.jahan@ymail.com>
2015-05-17 17:11:29 +0600

    second commit

---
+++ src/MyGitClass.java
@@ -0,0 +1,13 @@
+
+public class MyGitClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("my first project");
+
+	}
+
+}
